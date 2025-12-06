82bb8b800d328868f4ba7deac6e767ab36df84f8
Rafał Janiczek <rafal.janiczek@int.pl>
2025-12-06 12:39:11 +0100

    06 - shorter

--- 06.php
+++ 06.php
@@ -9,20 +9,14 @@ function input($input)
 
 function part1($input)
 {
-    $input = array_map(function ($l) {
-        preg_match_all('/\S+/', $l, $ret);
-        return $ret[0];
-    }, $input);
+    $input = array_map(fn($l) => preg_split('/\s+/', $l), $input);
 
     $func = array_pop($input);
 
     $sum = 0;
     foreach ($func as $id => $op) {
-        if ($op === '*') {
-            $sum += array_product(array_column($input, $id));
-        } else {
-            $sum += array_sum(array_column($input, $id));
-        }
+        $nums = array_column($input, $id);
+        $sum += $op === '*' ? array_product($nums) : array_sum($nums);
     }
 
     return $sum; //4951502530386
@@ -31,29 +25,19 @@ function part1($input)
 function part2($input)
 {
     $input = array_map(fn($l) => str_split($l), $input);
-
     $func = array_pop($input);
-
     $max = max(array_map(fn($l) => count($l), $input));
+
     $nums = [];
     $sum = 0;
-    $lastFunc = '';
     for ($id = 0; $id <= $max; $id++) {
-        $op = $func[$id] ?? '';
-        if (!empty(trim($op))) {
-            $lastFunc = $op;
-        }
+        $op = !empty(trim($func[$id]??'')) ?  $func[$id] : $op;
 
         $num = implode('', array_column($input, $id));
 
         if (trim($num) === '') {
-            if ($lastFunc === '*') {
-                $sum += array_product($nums);
-            } else {
-                $sum += array_sum($nums);
-            }
+            $sum += $op === '*' ? array_product($nums) : array_sum($nums);
             $nums = [];
-            $lastFunc = '';
             continue;
         }
 
